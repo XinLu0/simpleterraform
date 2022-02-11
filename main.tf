d6d14fc3fb35a69259ac346eec93ce33c4d1a8e7
xintong <xlu@topcon.com>
2022-02-11 11:41:28 +1000

    update

---
+++ main.tf
@@ -0,0 +1,97 @@
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  name   = "example-ec2-complete"
+  region = "ap-southeast-2"
+
+  user_data = <<-EOT
+  #!/bin/bash
+  echo "Hello Terraform!"
+  EOT
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = local.name
+  cidr = "10.99.0.0/18"
+
+  azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
+  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
+  database_subnets = ["10.99.7.0/24", "10.99.8.0/24", "10.99.9.0/24"]
+
+  tags = local.tags
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = local.name
+  description = "Security group for example usage with EC2 instance"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "all-icmp"]
+  egress_rules        = ["all-all"]
+
+  tags = local.tags
+}
+
+resource "aws_placement_group" "web" {
+  name     = local.name
+  strategy = "cluster"
+}
+
+resource "aws_kms_key" "this" {
+}
+
+resource "aws_network_interface" "this" {
+  subnet_id = element(module.vpc.private_subnets, 0)
+}
+
+################################################################################
+# EC2 Module
+################################################################################
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+#   key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = [module.security_group.security_group_id]
+  subnet_id              = element(module.vpc.private_subnets, 0)
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
